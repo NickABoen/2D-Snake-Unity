524cdcfa1267ae80b5b38637fc884c6af737f856
NickABoen <NickABoen@gmail.com>
2016-10-05 00:43:53 -0500

    Add some basic methods for instantiating prefabs

--- Assets/Scripts/MapSystem.cs
+++ Assets/Scripts/MapSystem.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject Background;
+    public GameObject Wall;
+    public Vector2 Map_Size;
+
+    private List<GameObject> Current_Map;
+
+    public void Start()
+    {
+        Current_Map = new List<GameObject>();
+    }
+
+    public void GenerateBasicMap()
+    {
+
+    }
+
+    private GameObject CreateBackground()
+    {
+        GameObject new_background = Instantiate(Background);
+
+        new_background.transform.localScale.Set(Map_Size.x, Map_Size.y, new_background.transform.localScale.z);
+        new_background.transform.position.Set(Map_Size.x / 2, Map_Size.y / 2, new_background.transform.position.z);
+
+        return new_background;
+    }
+
+    private GameObject CreateWall(Vector2 position)
+    {
+        GameObject new_wall = Instantiate(Wall);
+        new_wall.transform.position.Set(position.x, position.y, new_wall.transform.position.z);
+        return new_wall;
+    }
+
+    public void ClearMap()
+    {
+        Current_Map.Clear();
+    }
 }
