67d68d8a808423920fefa5cb9ade4e605880c6a4
NickABoen <NickABoen@gmail.com>
2016-10-08 19:57:47 -0500

    Squashed commit of the following:
    commit a5afe1c838367536b7573a69dab82d00d0a2ffd1
    Merge: ed84763 d8a3b2e
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Sat Oct 8 19:47:39 2016 -0500
    
        Resolve merge conflict with basic map generation
    
    commit ed84763a64cdce58e7c80d7558240755efc9e4c3
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Sat Oct 8 19:31:01 2016 -0500
    
        Enable Tile_Size usage in map generation
    
    commit 03105c8f35af695b396a82f932d06f23a874aae0
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Sat Oct 8 19:11:12 2016 -0500
    
        Line up bigger tile sizes with top left
    
    commit 5553d6b1f705831e3dcd9c9a3fab72afc137c5a1
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Sat Oct 8 19:09:35 2016 -0500
    
        Adjust y offset to fit on screen
    
    commit 7916a4445942bf6bca2bf7271e2b37a0805e2816
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Fri Oct 7 22:35:58 2016 -0500
    
        Set tile and camera sizes to Tile_Size
    
    commit 888852ce48cdc728dee717f69f42e216eb178ee7
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Fri Oct 7 21:28:57 2016 -0500
    
        Update scene
    
    commit 4800587f0dbf7e38e6255c9bb551bb35a7cd91cf
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Thu Oct 6 02:00:11 2016 -0500
    
        Improve border generation algorithm by mirroring
    
    commit d8a3b2ed46142a61d89298bc3bd8d1d802550f9b
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Thu Oct 6 02:00:11 2016 -0500
    
        Improve border generation algorithm using mirroring
    
    commit 6ef62c9e240075fa01c2857594504a2fb4927d2b
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Wed Oct 5 20:52:55 2016 -0500
    
        Update prefab default positions to be {0,0,0}
    
    commit ba604249046b8488ac3e7284885c19aad5469232
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Wed Oct 5 01:48:18 2016 -0500
    
        Generate maps on start and adjust camera to fit any scale
    
    commit cb799e6d751d4891c50c404b0e56f35a6d24c379
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Wed Oct 5 01:01:28 2016 -0500
    
        Ready map system for testing
    
    commit 524cdcfa1267ae80b5b38637fc884c6af737f856
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Wed Oct 5 00:43:53 2016 -0500
    
        Add some basic methods for instantiating prefabs
    
    commit 8d5ba91d10d6bf41f37f813fc8ba97b2545ad8e9
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Wed Oct 5 00:14:41 2016 -0500
    
        Add shell script for generating the map
    
    commit 682ab22b6769cf8ed6004872d1a30f54ed32acc1
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Tue Oct 4 23:49:43 2016 -0500
    
        Add background and wall prefabs
    
    commit 7cd4b7e6e5d440cffbb7af449276353c90e3a909
    Author: NickABoen <NickABoen@gmail.com>
    Date:   Tue Oct 4 23:32:32 2016 -0500
    
        Add sprite for tiles

---
+++ Assets/Scripts/MapSystem.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapSystem : MonoBehaviour {
+
+    public GameObject Background;
+    public GameObject Wall;
+    public Vector2 Map_Size = new Vector2(10.0f, 10.0f);
+    public Vector2 Tile_Size = new Vector2(1.0f, 1.0f);
+
+    private List<GameObject> Current_Map;
+
+    public void Start()
+    {
+        Current_Map = new List<GameObject>();
+        //Camera.main.orthographicSize = Mathf.Max(Map_Size.x / 2, Map_Size.y / 2);
+        Vector2 new_camera_size = Map_Size;
+        new_camera_size.Scale(Tile_Size);
+        new_camera_size = new_camera_size / 2;
+        Camera.main.orthographicSize = Mathf.Max(new_camera_size.x, new_camera_size.y);
+        GenerateBasicMap();
+    }
+
+    public void GenerateBasicMap()
+    {
+        Vector2 byX, byY;
+        byX = byY = Vector2.zero;
+        Vector2 offset_initial = Map_Size;
+        offset_initial.Scale(Tile_Size);
+        Vector2 offset = (offset_initial / 2);
+        offset.Scale(Vector2.left + Vector2.up);
+        offset.y -= Tile_Size.y;b
+
+        int max = (int)(Map_Size.x + Map_Size.y - 1);
+        for(int i = 0; i < max; i++)
+        {
+            bool vectorsEqual = Vector2.SqrMagnitude(byX - byY) == 0;
+
+            Vector2 byX_wall = byX;
+            byX_wall.Scale(Tile_Size);
+            byX_wall += offset;
+            CreateWall(byX_wall);
+
+            if(!vectorsEqual)
+            {
+                Vector2 byY_wall = byY;
+                byY_wall.Scale(Tile_Size);
+                byY_wall += offset;
+                CreateWall(byY_wall);
+            }
+
+            byX += (Mathf.Abs(byX.x) < Map_Size.x-1) ? Vector2.right : Vector2.down;
+            byY += (Mathf.Abs(byY.y) < Map_Size.y-1) ? Vector2.down : Vector2.right;
+        }
+
+        CreateBackground();
+    }
+
+    private void CreateBackground()
+    {
+        GameObject new_background = Instantiate(Background);
+
+        Vector2 background_size = Map_Size;
+        background_size.Scale(Tile_Size);
+        new_background.transform.localScale = background_size;
+
+        Current_Map.Add(new_background);
+    }
+
+    private void CreateWall(Vector2 position)
+    {
+        GameObject new_wall = Instantiate(Wall);
+        new_wall.transform.localScale = Tile_Size;
+        new_wall.transform.position = position + (Tile_Size / 2);
+        Current_Map.Add(new_wall);
+    }
+
+    public void ClearMap()
+    {
+        Current_Map.Clear();
+    }
+}
