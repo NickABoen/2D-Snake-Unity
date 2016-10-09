3bdbfeaf0ea01a3f8ae3ca19042bd0d25492c38a
NickABoen <NickABoen@gmail.com>
2016-10-08 22:09:37 -0500

    Complete basic head component script

--- Assets/Scripts/HeadComponent.cs
+++ Assets/Scripts/HeadComponent.cs
@@ -5,4 +5,23 @@ public class HeadComponent : MonoBehaviour {
     public Vector2 position;
     public Vector2 direction = Vector2.right;
     public TailComponent tail;
+
+    public int Size = 1;
+    private int current_size = 1;
+
+    public void UpdatePosition()
+    {
+        bool add_tail = current_size < Size;
+        if (tail != null)
+        {
+            tail.UpdatePosition(position, add_tail);
+        }
+        else if (add_tail)
+        {
+            tail = TailComponent.Spawn();
+            tail.UpdatePosition(position, false);
+        }
+        if (add_tail) current_size++;
+        position += direction;
+    }
 }
--- Assets/Scripts/TailComponent.cs
+++ Assets/Scripts/TailComponent.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class TailComponent : MonoBehaviour {
     public Vector2 position;
-    public GameObject tail_prefab;
+    public static GameObject tail_prefab;
     public TailComponent tail;
 
     public void UpdatePosition(Vector2 newPosition, bool add_tail)
@@ -15,16 +15,16 @@ public class TailComponent : MonoBehaviour {
         }
         else if (add_tail)
         {
-            tail = Spawn();
+            tail = TailComponent.Spawn();
+            tail.transform.position = position;
             tail.UpdatePosition(position, !add_tail);
         }
         position = newPosition;
     }
 
-    public TailComponent Spawn()
+    public static TailComponent Spawn()
     {
         TailComponent result = Instantiate(tail_prefab).GetComponent<TailComponent>();
-        result.transform.position = position;
 
         return result;
     }
