59ff5b8d341c5597a3694bbce86d0fba59439e48
NickABoen <NickABoen@gmail.com>
2016-10-08 23:12:16 -0500

    Add basic movement methods to player system

--- Assets/Scripts/PlayerSystem.cs
+++ Assets/Scripts/PlayerSystem.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class PlayerSystem : MonoBehaviour {
+    public HeadComponent playerHead;
+
+    private Vector2 temp_direction;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    public void MoveLeft()
+    {
+        temp_direction = Vector2.left;
+    }
+
+    public void MoveRight()
+    {
+        temp_direction = Vector2.right;
+    }
+
+    public void MoveUp()
+    {
+        temp_direction = Vector2.up;
+    }
+
+    public void MoveDown()
+    {
+        temp_direction = Vector2.down;
+    }
 }
