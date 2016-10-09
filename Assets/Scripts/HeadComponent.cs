7b77ef03f38842c103cb131b40466c25b13d742e
NickABoen <NickABoen@gmail.com>
2016-10-08 21:21:27 -0500

    Add Head Component class

---
+++ Assets/Scripts/HeadComponent.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadComponent : MonoBehaviour {
+    public Vector2 position;
+    public Vector2 direction = Vector2.right;
+    public TailComponent tail;
+}
