c9a854b907e58a0248317e380e9efe55dd7610b0
NickABoen <NickABoen@gmail.com>
2016-10-09 20:14:46 -0500

    Make TimerSystem outline in script

--- Assets/Scripts/TimerSystem.cs
+++ Assets/Scripts/TimerSystem.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TimerSystem : MonoBehaviour {
+    private List<TimerComponent> GeneralTimers;
+    private List<TimerComponent> DestructableTimers;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Awake()
+    {
+        GeneralTimers = new List<TimerComponent>();
+        DestructableTimers = new List<TimerComponent>();
+    }
+
+    void Update()
+    {
+        foreach(TimerComponent tc in GeneralTimers)
+        {
+        }
+        foreach(TimerComponent tc in DestructableTimers)
+        {
+        }
+    }
 }
