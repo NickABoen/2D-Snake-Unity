eb902b6321845a180ce06d95957feb2ca9c95ddb
NickABoen <NickABoen@gmail.com>
2016-10-14 01:29:46 -0500

    Add Follower Component with basic implementation

---
+++ Assets/Scripts/FollowerComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerComponent : MonoBehaviour {
+    public bool IsLeader;
+    public FollowerComponent Tail = null;
+    public Vector2 position
+    {
+        get { return gameObject.transform.position; }
+        set { gameObject.transform.position = value; }
+    }
+
+    public void UpdatePositions(Vector2 newPosition)
+    {
+        if(Tail != null)
+        {
+            Tail.UpdatePositions(this.position);
+        }
+
+        this.position = newPosition;
+    }
+}
