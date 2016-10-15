2dcac3331d58f8ea6086bf1a74b53dd7e7be2d6f
NickABoen <NickABoen@gmail.com>
2016-10-15 11:55:11 -0500

    Add head and body segment prefabs

--- Assets/Scripts/SegmentSystem.cs
+++ Assets/Scripts/SegmentSystem.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 public class SegmentSystem : MonoBehaviour {
     public GameObject Head_Prefab, Segment_Prefab;
+    public int Size;
 
     FollowerComponent Head, Tail;
     List<FollowerComponent> Segment_list;
     TimerSystem timer_system;
 
-    public int Size;
     int current_size;
 
     TimerComponent step_timer;
