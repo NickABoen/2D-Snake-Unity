93bdadcc75c5854acf91ea29269ef5bae01de1b4
NickABoen <NickABoen@gmail.com>
2016-10-15 18:11:18 -0500

    Change how direction works and add an enum for it

--- Assets/Scripts/SegmentSystem.cs
+++ Assets/Scripts/SegmentSystem.cs
@@ -2,6 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public enum Direction
+{
+    RIGHT,
+    DOWN,
+    LEFT,
+    UP
+}
+
 public class SegmentSystem : MonoBehaviour {
     public GameObject Head_Prefab, Segment_Prefab;
     public int Size;
@@ -13,11 +21,31 @@ public class SegmentSystem : MonoBehaviour {
     int current_size;
 
     TimerComponent step_timer;
-    Vector2 direction;
+    Direction direction;
 
     public void UpdatePositions()
     {
         AddSegment();
+
+        Vector2 direction_value;
+        switch (direction)
+        {
+            case Direction.UP:
+                direction_value = Vector2.up;
+                break;
+            case Direction.DOWN:
+                direction_value = Vector2.down;
+                break;
+            case Direction.LEFT:
+                direction_value = Vector2.left;
+                break;
+            case Direction.RIGHT:
+                direction_value = Vector2.right;
+                break;
+            default:
+                direction_value = Vector2.zero;
+                break;
+        }
 
         Vector2 next_position = Vector2.zero;
         Vector2 new_position = Vector2.zero;
@@ -25,7 +53,7 @@ public class SegmentSystem : MonoBehaviour {
         {
             if (segment.IsLeader)
             {
-                next_position = segment.position + direction;
+                next_position = segment.position + direction_value;
             }
 
             new_position = next_position;
@@ -42,9 +70,8 @@ public class SegmentSystem : MonoBehaviour {
         }
     }
 
-    void ChangeDirection(Vector2 newDirection)
+    void ChangeDirection(Direction newDirection)
     {
-
     }
 
     FollowerComponent CreateSegment()
