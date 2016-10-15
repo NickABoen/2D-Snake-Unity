e6129c3e048410a6178e2024ed188e717e17e30d
NickABoen <NickABoen@gmail.com>
2016-10-15 13:45:05 -0500

    Make component factory a static class

--- Assets/Scripts/ComponentFactory.cs
+++ Assets/Scripts/ComponentFactory.cs
@@ -1,15 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ComponentFactory : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public static class ComponentFactory { 
+    //Add public static creation methods here
 }
