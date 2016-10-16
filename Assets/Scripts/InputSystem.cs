5575ea567dca6901f15f15a78d1434780ac9456f
NickABoen <NickABoen@gmail.com>
2016-10-15 23:18:47 -0500

    Implement basic input system

---
+++ Assets/Scripts/InputSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputSystem : Singleton<InputSystem>{
+    public enum InputDirection { NONE, LEFT, RIGHT };
+    InputDirection Direction;
+
+	// Update is called once per frame
+	void Update () {
+        float input_value = Input.GetAxis("Horizontal");
+        if(input_value > 0)
+        {
+            Direction = InputDirection.RIGHT;
+        }
+        else if(input_value < 0)
+        {
+            Direction = InputDirection.LEFT;
+        }
+        else
+        {
+            Direction = InputDirection.NONE;
+        }
+	}
+}
