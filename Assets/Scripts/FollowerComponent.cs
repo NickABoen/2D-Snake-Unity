43947dfd5b01fefe697312b2b31d87b14cf239cd
NickABoen <NickABoen@gmail.com>
2016-10-15 20:09:29 -0500

    Add Follower component to segment prefabs and remove follower component updating

--- Assets/Scripts/FollowerComponent.cs
+++ Assets/Scripts/FollowerComponent.cs
@@ -3,20 +3,9 @@ using System.Collections;
 
 public class FollowerComponent : MonoBehaviour {
     public bool IsLeader;
-    public FollowerComponent Tail = null;
     public Vector2 position
     {
         get { return gameObject.transform.position; }
         set { gameObject.transform.position = value; }
-    }
-
-    public void UpdatePositions(Vector2 newPosition)
-    {
-        if(Tail != null)
-        {
-            Tail.UpdatePositions(this.position);
-        }
-
-        this.position = newPosition;
     }
 }
