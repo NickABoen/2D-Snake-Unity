4501dee622bcc6d3760ceb4a8a6106ca16cc1849
NickABoen <NickABoen@gmail.com>
2016-10-15 23:36:20 -0500

    Added threshold to input system, needs to be more responsive

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class InputSystem : Singleton<InputSystem>{
     public enum InputDirection { NONE, LEFT, RIGHT };
     public InputDirection Direction;
+    public float InputThreshold = 0.5f;
 
 	// Update is called once per frame
 	void Update () {
         float input_value = Input.GetAxis("Horizontal");
-        if(input_value > 0)
+        if(input_value > InputThreshold)
         {
             Direction = InputDirection.RIGHT;
         }
-        else if(input_value < 0)
+        else if(input_value < (-InputThreshold))
         {
             Direction = InputDirection.LEFT;
         }
